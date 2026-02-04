df2fd930315e1eee43bcf1c57dc1811193f8162d
smitk-tech <smit.k@york.ie>
2026-02-04 21:51:35 +0530

    feature: implement swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('General')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get hello world message' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a simple greeting.',
+    type: String,
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WorkSphere API')
+    .setDescription('The WorkSphere backend API documentation')
+    .setVersion('1.0')
+    .addTag('work-sphere')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
