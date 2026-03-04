8024ad90cdc8f19796162a2fa22a2dd332a4a68b
smitk-tech <smit.k@york.ie>
2026-03-04 17:40:47 +0530

    feature: test

--- src/modules/payment/controller/payment.controller.ts
+++ src/modules/payment/controller/payment.controller.ts
@@ -19,7 +19,7 @@ export class PaymentController {
 
   @Get('history')
   async getPaymentHistory(@Query('email') email?: string) {
-    console.log('email from payment controller', email);
+    console.log('email payment controller', email);
     if (email) {
       return this.paymentService.getPaymentHistoryByCustomerId(email);
     }
